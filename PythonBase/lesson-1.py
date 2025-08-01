c55cad6269b9fb828f65846897183fd7088b6922
boris <boris@LAPTOP-655K9220>
2025-08-01 20:05:49 +0200

    Типы данных в Python

--- PythonBase/lesson-1.py
+++ PythonBase/lesson-1.py
@@ -1,1 +1,36 @@
-print('Hello')
+num = 15
+# id() - выдает номер ячейки в памяти, где хранится переменная
+print(id(num))
+
+# Целое число / integer / int
+num = 20
+print(type(num))
+
+# Дробные числа / вещественные числа / числа с плавающей точкой / float
+fl_num = 3.14
+print(type(fl_num))
+
+# Строка / string / str
+str1 = 'Str1'
+str2 = "Str2"
+print(type(str1))
+
+# Список / list
+lt = [9, 'hello', 7.95]
+print(type(lt))
+
+# Кортеж / tuple
+tp = (67, 98, 'poka', 0.6)
+print(type(tp))
+
+# Множество / set
+st = {5, 7, 6, 5, 5, 7, 5, 6, 7, 'hello'}
+print(type(st))
+
+# Словарь / dictionary / dict
+client = {'name': 'Boris', 'height': 170, 'age': 27}
+print(type(client))
+
+# Логический тип / Булевый тип / boolean / bool
+logic = True
+print(type(logic))
