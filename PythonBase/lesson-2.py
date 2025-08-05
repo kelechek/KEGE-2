2d4e173917416beb1a76fc9b5053415ebf37113b
boris <boris@LAPTOP-655K9220>
2025-08-05 18:39:15 +0200

    Таблица перевода типов данных

---
+++ PythonBase/lesson-2.py
@@ -0,0 +1,9 @@
+# Таблица перевода типов данных
+# int -> float(), str(), bool()
+# float -> int(), str(), bool()
+# str -> int(), float(), bool(), list(), tuple(), set()
+# list -> str(), bool(), tuple(), set()
+# tuple -> str(), bool(), list(), set()
+# set -> str(), bool(), list(), tuple()
+# dict -> str(), bool(), list(), tuple(), set()
+# bool -> int(), float(), str()
