47f59a982fe3e64454f3f34b7be1bbae0ca008f7
boris <boris@LAPTOP-655K9220>
2025-08-08 19:31:22 +0200

    Работа с библиотеками

---
+++ PythonBase/lesson-5.py
@@ -0,0 +1,19 @@
+# Работа с библиотеками
+
+# Подключение библиотеки
+import random
+# Подключение библиотеки через псевдоним
+import random as rn
+# Подключение одной команды из библиотеки
+from random import randint
+# Подключение нескольких команд из библиотеки
+from random import randint, choice
+# Подключение всех команд из библиотеки
+from random import *
+# Подключение одной команды из библиотеки через псевдоним
+from random import randint as rn
+# Подключение нескольких команд из библиотеки через псевдонимы
+from random import randint as rn, choice as ch
+
+
+
